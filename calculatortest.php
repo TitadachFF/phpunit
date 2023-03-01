83c82630c690c35c9431f4391a20cc76fc1e3303
TitadachFF <120708254+TitadachFF@users.noreply.github.com>
2023-03-01 14:48:12 +0700

    Initial commit

---
+++ calculatortest.php
@@ -0,0 +1,22 @@
+<?php
+include "calculator.php";
+
+use PHPUnit\Framework\TestCase;
+
+class calculatortest extends TestCase {
+
+
+    public function testAddNumberPositive(){
+        $c1 = new Calculator();
+        $this -> assertEquals(14,$c1->addNumber(5,9));
+        $this -> assertEquals(57,$c1->addNumber(5,52));
+    }
+    public function testAddNumberNegative(){
+        $c1 = new Calculator();
+        $this -> assertEquals(-4,$c1->addNumber(5,-9));
+        $this -> assertEquals(-107,$c1->addNumber(-55,-52));
+    }
+    
+}
+
+?>
